09706278a94614dd0b4f1e0caff94336d688b695
luyn <102026801+Luyn2161@users.noreply.github.com>
2022-06-21 07:14:39 +0700

    1

--- react practice f8/my-app/src/App.js
+++ react practice f8/my-app/src/App.js
@@ -1,31 +1,25 @@
-import { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    const [checked, setChecked] = useState([]);
-
-    console.log(checked);
-    console.log("rerender");
-    const courses = [
-        { id: 1, name: "java" },
-        { id: 2, name: "c++" },
-        { id: 3, name: "javascript" },
-    ];
-    const handleCheck = (id) => {
-        setChecked([id]);
-    };
-    return (
-        <div className="App">
-            {courses.map((course) => (
-                <div key={course.id}>
-                    <input
-                        type="checkbox"
-                        onChange={() => handleCheck(course.id)}
-                    />
-                    {course.name}
-                </div>
-            ))}
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
