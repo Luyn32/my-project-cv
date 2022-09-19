f1ccf5580017bd390a69963563908422aa8978f1
luyn <102026801+Luyn2161@users.noreply.github.com>
2022-09-20 01:08:51 +0700

    js copy first

--- javascript practice f8/javascript/app.js
+++ javascript practice f8/javascript/app.js
@@ -1,16 +1,1 @@
-var listCoursesBlock = document.querySelector("list-courses");
-var courseApi = "http://localhost:3000/courses";
-function start() {
-    getCourses(function (courses) {
-        console.log(courses);
-    });
-}
-start();
-//function
-function getCourses(callback) {
-    fetch(course)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(callback);
-}
+
